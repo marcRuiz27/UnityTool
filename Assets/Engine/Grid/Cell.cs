bbb38c42ab758c7282efd17ad4481c7971df2abe
XbraxyDev <ruizmp27@gmail.com>
2026-01-21 21:00:41 +0100

    Free sprites and tile assets imported

--- Assets/Engine/Grid/Cell.cs
+++ Assets/Engine/Grid/Cell.cs
@@ -6,9 +6,11 @@ namespace Assets.Engine.Grid
 {
     internal class Cell
     {
+        [SerializeField]
         public Vector2Int gridPos;
+        [SerializeField]
         public Vector3 worldPos;
-
+        
 
         public bool isEmpty;
         public int moveCost;
---
+++ Assets/Engine/Grid/MyGridBuilder.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Engine.Grid
+{
+    internal class MyGridBuilder : MonoBehaviour
+    {
+
+
+        UnityEngine.Grid _LvLGrid;
+        Tilemap[] _tileMaps;
+        GridInformation _gridInformation;
+
+        //public MyGrid CreateMyGridByTileMap()
+        //{
+           
+        //}
+
+
+
+    }
+}
